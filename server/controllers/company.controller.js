9cf5e65c82e41697e30bcc95a581eb4a7f44210c
ZainHanif29 <zainhanif2902@gmail.com>
2025-02-27 18:13:18 +0500

    company : controller,route & API testing

---
+++ server/controllers/company.controller.js
@@ -0,0 +1,110 @@
+import { Company } from "../models/company.model.js";
+
+export const registerCompany = async (req, res) => {
+  try {
+    const { companyName } = req.body;
+    if (!companyName) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Company name is required." });
+    }
+    let company = await Company.findOne({ name: companyName });
+    if (company) {
+      return res
+        .status(400)
+        .json({ success: false, message: "You can't register same company." });
+    }
+    company = await Company.create({
+      name: companyName,
+      userId: req.id,
+    });
+    return res.status(201).json({
+      company,
+      success: true,
+      message: "Company registered successfully.",
+    });
+  } catch (error) {
+    console.log(`Internal Server Error in Register-Company API: ${error} `);
+    return res.status(500).json({
+      success: false,
+      message: `Internal Server Error in Register-Company API: ${error} `,
+    });
+  }
+};
+
+export const getCompany = async (req, res) => {
+  try {
+    const userId = req.id;
+    const companies = await Company.find({ userId });
+    if (!companies) {
+      return res.status(404).json({
+        success: false,
+        message: `Companies not found`,
+      });
+    }
+    return res.status(200).json({
+      companies,
+      success: true,
+      message: `Company found successfully`,
+    });
+  } catch (error) {
+    console.log(`Internal Server Error in Get-Company API: ${error} `);
+    return res.status(500).json({
+      success: false,
+      message: `Internal Server Error in Get-Company API: ${error} `,
+    });
+  }
+};
+
+export const getCompanyById = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+    if (!company) {
+      return res.status(404).json({
+        success: false,
+        message: `Company not found`,
+      });
+    }
+    return res.status(200).json({
+      company,
+      success: true,
+      message: `Company found successfully`,
+    });
+  } catch (error) {
+    console.log(`Internal Server Error in Get-Company-By-Id API: ${error} `);
+    return res.status(500).json({
+      success: false,
+      message: `Internal Server Error in Get-Company-By-Id API: ${error} `,
+    });
+  }
+};
+
+export const updateCompany = async (req, res) => {
+  try {
+    const { name, description, website, location } = req.body;
+    const file = req.file;
+    //   cloudinary
+
+    const updateData = { name, description, website, location };
+    const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
+      new: true,
+    });
+    if (!company) {
+      return res.status(200).json({
+        success: false,
+        message: `Company not found`,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: `Company information updated successfully`,
+    });
+  } catch (error) {
+    console.log(`Internal Server Error in Update-Company API: ${error} `);
+    return res.status(500).json({
+      success: false,
+      message: `Internal Server Error in Update-Company API: ${error} `,
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 
 dotenv.config({});
 const app = express();
@@ -19,6 +20,7 @@ app.use(cors(corsOption));
 
 // API
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
